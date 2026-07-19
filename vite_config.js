cebea299e1ce4766f40de043d47d0ccdb8c14a5b
Dartcleia Moura Martins Neves <dartmourapsi@gmail.com>
2026-07-19 10:46:44 -0300

    Add files via upload

---
+++ vite_config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// IMPORTANT: troque "NOME-DO-REPOSITORIO" pelo nome exato do seu repositório
+// no GitHub antes de publicar. Se o repo se chamar "myrella-15-anos", fica:
+// base: "/myrella-15-anos/"
+export default defineConfig({
+  plugins: [react()],
+  base: "/NOME-DO-REPOSITORIO/",
+});
